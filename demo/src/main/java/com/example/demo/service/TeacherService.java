3e70a8c056bad5359ad320cb8d05146a1f75941c
liangzhixin <zhixin.liang@jieshunpay.cn>
2018-10-19 08:54:08 +0800

    modify

--- demo/src/main/java/com/example/demo/service/TeacherService.java
+++ demo/src/main/java/com/example/demo/service/TeacherService.java
@@ -5,6 +5,7 @@ import com.example.demo.entity.Teacher;
 import com.example.demo.mapper.TeacherMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -13,13 +14,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@CacheConfig(cacheNames = "tea",keyGenerator = "keyGenerator")
 public class TeacherService {
 
     @Autowired
     private TeacherMapper teacherMapper;
 
     /**
-     * @Cacheable注解: 如果缓存没有值,从则执行方法并缓存数据(返回值),如果缓存有值,则从缓存中获取值
+     * @Cacheable注解:
+     *      如果缓存没有值,从则执行方法并缓存数据(返回值),如果缓存有值,则从缓存中获取值
+     *      判断缓存中有没有值: 其实是判断第一次生成的key在不在缓存中,在的话根据key取值,不在的话缓存数据
      *  1.value: 等价于cacheNames
      *  2.cacheNames: 指定缓存的名称
      *  3.key: 使用spring El表达式定义key,不指定会使用全部的参数计算一个key值,与keyGenerator不能同时使用
@@ -46,14 +50,22 @@ public class TeacherService {
      *  3.beforeInvocation:
      *    beforeInvocation=true: 在方法还没有执行的时候就清空缓存
      *    beforeInvocation=false(默认): 方法执行完清空缓存(如果方法执行抛出异常，则不会清空缓存)
+     *
+     *
+     *
+     * @CacheConfig注解: 设置类级别的相关属性,在类上使用
+     *  1.cacheNames: 类中的缓存操作使用的默认缓存的名称
+     *  2.keyGenerator: 设置类中key的生成策略
      */
 
-//    @Cacheable(value = "tea",keyGenerator = "keyGenerator")
+    @Cacheable
 //    @CachePut(value = "tea",keyGenerator = "keyGenerator")
-    @CacheEvict(value = "tea",keyGenerator = "keyGenerator",beforeInvocation = true)
+//    @CacheEvict(value = "tea",keyGenerator = "keyGenerator",beforeInvocation = true)
     public Teacher getTeacher(){
-        int i = 1/0;
+//        int i = 1/0;
         log.info("执行了此方法...");
         return teacherMapper.selectById(2);
+//        return null;  //如果设置了不缓存null值,这里返回null那么缓存数据时会报错
     }
+
 }
