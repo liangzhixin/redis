a67c1600681262c29f2c7941140d805912320dd6
liangzhixin <zhixin.liang@jieshunpay.cn>
2018-10-19 08:56:59 +0800

    modify

--- demo/src/main/java/com/example/demo/service/TeacherService.java
+++ demo/src/main/java/com/example/demo/service/TeacherService.java
@@ -23,7 +23,7 @@ public class TeacherService {
     /**
      * @Cacheable注解:
      *      如果缓存没有值,从则执行方法并缓存数据(返回值),如果缓存有值,则从缓存中获取值
-     *      判断缓存中有没有值: 其实是判断第一次生成的key在不在缓存中,在的话根据key取值,不在的话缓存数据
+     *      判断缓存中有没有值: 其实是判断key在不在缓存中,在的话根据key取值,不在的话缓存数据
      *  1.value: 等价于cacheNames
      *  2.cacheNames: 指定缓存的名称
      *  3.key: 使用spring El表达式定义key,不指定会使用全部的参数计算一个key值,与keyGenerator不能同时使用
@@ -34,6 +34,7 @@ public class TeacherService {
      *    sync=false(默认): 跟true相反,不会加锁
      *  6.condition: 在执行方法前，condition的值为true，则缓存数据
      *  7.unless: 在执行方法后，condition的值为true，则不缓存数据
+     *  ...
      *
      *
      *
@@ -50,12 +51,14 @@ public class TeacherService {
      *  3.beforeInvocation:
      *    beforeInvocation=true: 在方法还没有执行的时候就清空缓存
      *    beforeInvocation=false(默认): 方法执行完清空缓存(如果方法执行抛出异常，则不会清空缓存)
+     *  ...
      *
      *
      *
      * @CacheConfig注解: 设置类级别的相关属性,在类上使用
      *  1.cacheNames: 类中的缓存操作使用的默认缓存的名称
      *  2.keyGenerator: 设置类中key的生成策略
+     *  ...
      */
 
     @Cacheable
